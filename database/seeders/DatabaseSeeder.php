5f4c19c924bdcc6281a462a1d1bef1fafb455f62
Saeed <saeed17@gmail.com>
2023-04-22 14:48:38 +0100

    Seeder for test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'admin@test.com',
+            'email_verified_at' => null,
+        ]);
     }
 }
